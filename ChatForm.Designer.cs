29e542584fbadd962fa91dd4c6d9be928f7f22c5
DiegoRS2 <72531642+DiegoRS2@users.noreply.github.com>
2023-06-10 21:29:40 -0300

    design

---
+++ ChatForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace FrontChat
+{
+    partial class FluentDesignForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            fluentDesignFormControl1 = new DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl();
+            Chat = new DevExpress.XtraBars.FluentDesignSystem.FluentFormDefaultManager(components);
+            layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            layoutControl2 = new DevExpress.XtraLayout.LayoutControl();
+            layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            layoutControl3 = new DevExpress.XtraLayout.LayoutControl();
+            layoutControlGroup2 = new DevExpress.XtraLayout.LayoutControlGroup();
+            ((System.ComponentModel.ISupportInitialize)fluentDesignFormControl1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Chat).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControl1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Root).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControl2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControlGroup1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControl3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControlGroup2).BeginInit();
+            SuspendLayout();
+            // 
+            // fluentDesignFormControl1
+            // 
+            fluentDesignFormControl1.FluentDesignForm = this;
+            fluentDesignFormControl1.Location = new Point(0, 0);
+            fluentDesignFormControl1.Manager = Chat;
+            fluentDesignFormControl1.Name = "fluentDesignFormControl1";
+            fluentDesignFormControl1.Size = new Size(691, 31);
+            fluentDesignFormControl1.TabIndex = 2;
+            fluentDesignFormControl1.TabStop = false;
+            // 
+            // Chat
+            // 
+            Chat.Form = this;
+            // 
+            // layoutControl1
+            // 
+            layoutControl1.Dock = DockStyle.Left;
+            layoutControl1.Location = new Point(0, 31);
+            layoutControl1.Name = "layoutControl1";
+            layoutControl1.Root = Root;
+            layoutControl1.Size = new Size(164, 442);
+            layoutControl1.TabIndex = 3;
+            // 
+            // Root
+            // 
+            Root.AppearanceGroup.BackColor = Color.MidnightBlue;
+            Root.AppearanceGroup.Options.UseBackColor = true;
+            Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            Root.GroupBordersVisible = false;
+            Root.Name = "Root";
+            Root.Size = new Size(164, 442);
+            Root.TextVisible = false;
+            // 
+            // layoutControl2
+            // 
+            layoutControl2.Dock = DockStyle.Top;
+            layoutControl2.Location = new Point(164, 31);
+            layoutControl2.Name = "layoutControl2";
+            layoutControl2.Root = layoutControlGroup1;
+            layoutControl2.Size = new Size(527, 62);
+            layoutControl2.TabIndex = 4;
+            layoutControl2.Text = "layoutControl2";
+            // 
+            // layoutControlGroup1
+            // 
+            layoutControlGroup1.AppearanceGroup.BackColor = Color.FromArgb(224, 224, 224);
+            layoutControlGroup1.AppearanceGroup.Options.UseBackColor = true;
+            layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            layoutControlGroup1.GroupBordersVisible = false;
+            layoutControlGroup1.Name = "layoutControlGroup1";
+            layoutControlGroup1.Size = new Size(527, 62);
+            layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControl3
+            // 
+            layoutControl3.Dock = DockStyle.Fill;
+            layoutControl3.Location = new Point(164, 93);
+            layoutControl3.Name = "layoutControl3";
+            layoutControl3.Root = layoutControlGroup2;
+            layoutControl3.Size = new Size(527, 380);
+            layoutControl3.TabIndex = 5;
+            layoutControl3.Text = "layoutControl3";
+            // 
+            // layoutControlGroup2
+            // 
+            layoutControlGroup2.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            layoutControlGroup2.GroupBordersVisible = false;
+            layoutControlGroup2.Name = "layoutControlGroup2";
+            layoutControlGroup2.Size = new Size(527, 380);
+            layoutControlGroup2.TextVisible = false;
+            // 
+            // FluentDesignForm1
+            // 
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(691, 473);
+            Controls.Add(layoutControl3);
+            Controls.Add(layoutControl2);
+            Controls.Add(layoutControl1);
+            Controls.Add(fluentDesignFormControl1);
+            FluentDesignFormControl = fluentDesignFormControl1;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FluentDesignForm1";
+            Text = "Chat";
+            ((System.ComponentModel.ISupportInitialize)fluentDesignFormControl1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Chat).EndInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControl1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Root).EndInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControl2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControlGroup1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControl3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)layoutControlGroup2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl fluentDesignFormControl1;
+        private DevExpress.XtraBars.FluentDesignSystem.FluentFormDefaultManager Chat;
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraLayout.LayoutControl layoutControl3;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup2;
+        private DevExpress.XtraLayout.LayoutControl layoutControl2;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+    }
+}
